9a835f69f09e1f43c9fc870ca28fc306e6348ad1
Sujal Rajput <rajputsujal992@gmail.com>
2023-09-13 19:15:56 +0530

    public and private key

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,31 +1,56 @@
-const model = require('../Model/User');
+const model = require("../Model/User");
+const fs = require("fs");
+const path = require("path");
+const privateKey = fs.readFileSync(
+  path.resolve(__dirname, "../private.key"),
+  "utf-8"
+);
+const publicKey = fs.readFileSync(
+  path.resolve(__dirname, "../public.key"),
+  "utf-8"
+);
 const Users = model.Users;
-const jwt =  require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-exports.createUser = async(req,res) => {
-    const user = new Users(req.body);
-    let token = jwt.sign({email:req.body.userEmail},'hhhhh');
-    user.token= token;
-    try {
-        const createdUser = await user.save();
-        res.json({token:createdUser.token});
-    } catch (error) {
-        res.json(error);
-    }
-}
+exports.createUser = async (req, res) => {
+  const user = new Users(req.body);
+  let token = jwt.sign({ email: req.body.userEmail }, privateKey, {
+    algorithm: "RS256",
+  });
+  user.token = token;
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  user.password = hash;
+  try {
+    const createdUser = await user.save();
+    res.json({ token: createdUser.token });
+  } catch (error) {
+    res.json(error);
+  }
+};
 
-exports.verifyUser = async(req,res) => {
-    const token = req.get("Authorization").split('Bearer ')[1];
-    // console.log(token);
-    console.log('errorr');
-    try {
-        const decoded = jwt.verify(token, 'hhhhh');
-        if(decoded.email){
-            const user = await Users.findOne({userEmail:decoded.email});
-            // console.log(user.userEmail);
-            res.json({userName:user.userName, userEmail:user.userEmail, orders:user.orders,cart:user.cart,"authorise":true});
-        }
-    } catch (error) {
-        res.status(401).json({"authorise":false});
+exports.verifyUser = async (req, res) => {
+  const token = req.get("Authorization").split("Bearer ")[1];
+  // console.log(token);
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    if (decoded.email) {
+      const user = await Users.findOne({ userEmail: decoded.email });
+      // console.log(user.userEmail);
+      res.json({
+        userName: user.userName,
+        userEmail: user.userEmail,
+        orders: user.orders,
+        cart: user.cart,
+        authorise: true,
+      });
     }
-}
+  } catch (error) {
+    res.json({ authorise: false });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+
+    bcrypt.compareSync(myPlaintextPassword, hash);
+};
