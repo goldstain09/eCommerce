0159abaab8133eb748a04248426065559c2fcba5
Sujal Rajput <rajputsujal992@gmail.com>
2023-10-24 12:02:21 +0530

    Process.env changes for deployment

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const path = require('path');
 const userRoute = require('./Routes/User');
 const productsRoute = require('./Routes/Products');
 const sellerRoute = require('./Routes/Seller');
+require('dotenv').config();
 
 const server = express();
-console.log("server started");
+// console.log("server started");
 
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://rajputsujal992:YbJuMa2wjJ155fIY@cluster0.4wvygwc.mongodb.net/ecommerce?retryWrites=true&w=majority');
-  console.log('db connected');
+  await mongoose.connect(process.env.MONGO);
+  // console.log('db connected');
 }
 
 
 server.use(cors());
 server.use(express.json()); 
-server.use(express.static('build'));
+server.use(express.static(process.env.STATIC));
 server.use('/user',userRoute.Routes);
 server.use('/productsApi',productsRoute.Routes);
 server.use('/seller',sellerRoute.Routes);
@@ -31,5 +32,5 @@ server.use('*',(req,res)=>{
 
 
 
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
